e83dee6d923d4968e88c5f487d277e0382ce10c7
EB-303 <wildeye.eb@gmail.com>
2025-06-16 19:06:34 +0300

    python executor works

--- python-executor/app.py
+++ python-executor/app.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 @app.route('/execute', methods=['POST'])
 def execute():
-    data = request.json or {} 
-    return jsonify({"recived: " data}), 200
+    data = request.json or {}
+    return jsonify({"received": data}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
