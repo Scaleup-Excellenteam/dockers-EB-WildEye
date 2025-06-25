e422a0c734dc94f573112c291699019260c76348
EB-303 <wildeye.eb@gmail.com>
2025-06-26 01:05:49 +0300

    local host work properly after debuging

--- router/app.py
+++ router/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS 
 import requests
 import os
 import tempfile
 import uuid
 
 app = Flask(__name__)
+CORS(app)
 
 # Store file mappings
 file_storage = {}
